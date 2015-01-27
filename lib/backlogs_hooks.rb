f32e6c4d062f2754ca9eedb99ba5bd07ce7b049d
Viktor Masicek <viktor@masicek.net>
2015-01-27 16:05:12 +0100

    issue show: show releases for all tracker type

--- lib/backlogs_hooks.rb
+++ lib/backlogs_hooks.rb
@@ -99,12 +99,13 @@ module BacklogsPlugin
             vbe = issue.velocity_based_estimate
             snippet += "<tr><th>#{l(:field_velocity_based_estimate)}</th><td>#{vbe ? vbe.to_s + ' days' : '-'}</td></tr>"
 
-            unless issue.release_id.nil?
-              release = RbRelease.find(issue.release_id)
-              snippet += "<tr><th>#{l(:field_release)}</th><td>#{link_to(release.name, url_for_prefix_in_hooks + url_for({:controller => 'rb_releases', :action => 'show', :release_id => release}))}</td>"
-              relation_translate = l("label_release_relationship_#{RbStory.find(issue.id).release_relationship}")
-              snippet += "<th>#{l(:field_release_relationship)}</th><td>#{relation_translate}</td></tr>"
-            end
+          end
+
+          unless issue.release_id.nil?
+            release = RbRelease.find(issue.release_id)
+            snippet += "<tr><th>#{l(:field_release)}</th><td>#{link_to(release.name, url_for_prefix_in_hooks + url_for({:controller => 'rb_releases', :action => 'show', :release_id => release}))}</td>"
+            relation_translate = l("label_release_relationship_#{RbStory.find(issue.id).release_relationship}")
+            snippet += "<th>#{l(:field_release_relationship)}</th><td>#{relation_translate}</td></tr>"
           end
 
           if issue.is_task? && User.current.allowed_to?(:update_remaining_hours, project) != nil
